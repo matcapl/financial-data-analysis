d3823a5c9c2d81e283cd480caaad359895a80775
matcapl <adam@mather.capital>
2025-08-01 11:51:45 +0100

    Latest Dockerfile and index.js for troubleshooting

--- server/api/index.js
+++ server/api/index.js
@@ -13,7 +13,7 @@ const uploadRouter = require('./upload');
 const reportRouter = require('./generate-report');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 
