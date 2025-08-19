476ad885e9f770b91dbcf9bc55024801aec092ab
matcapl <adam@mather.capital>
2025-08-19 09:58:39 +0100

    Delete duplicate blob env call

--- server/api/generate-report.js
+++ server/api/generate-report.js
@@ -1,6 +1,6 @@
 const { spawn } = require('child_process');
 const { put } = require('@vercel/blob');
-const BLOB_TOKEN = process.env.BLOB_READ_WRITE_TOKEN || process.env.VERCEL_BLOB_TOKEN;
+const BLOB_TOKEN = process.env.VERCEL_BLOB_TOKEN;
 const path = require('path');
 const fs = require('fs');
 
